0a315d44b73de99ab65ba5d9a8de411ee3c01224
Jacopo Penazzi <j.penazzi@cineca.it>
2020-01-28 11:24:12 +0100

    removed apikey

--- src/app/features/search/components/gmap/gmap.component.ts
+++ src/app/features/search/components/gmap/gmap.component.ts
@@ -5,7 +5,7 @@ import {Hotel} from '../../../../model/hotel';
   selector: 'app-gmap',
   template: `
     <div>
-      <img src="https://maps.googleapis.com/maps/api/staticmap?center={{active?.location.address}}&zoom=15&size=700x200&key=AIzaSyB7fSvdkeXMnRlT86uiD3dHNKMbH4EM4Ws"
+      <img src="https://maps.googleapis.com/maps/api/staticmap?center={{active?.location.address}}&zoom=15&size=700x200&key="
            width="100%" height="200">
       <div class="address">
         <div class="font-big">{{active?.name}}</div>
