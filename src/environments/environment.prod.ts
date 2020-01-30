d86c8abdd80aa49a294d5e07f796a90890d2db8b
jack982 <jack982@users.noreply.github.com>
2020-01-31 00:35:58 +0100

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  API_HOTELS_URL: 'http://localhost:3000/hotels',
-  API_LOGIN_URL: 'http://localhost:3000/login',
-  API_MAP_KEY: 'AIzaSyCs03y3kEzvz11FVokEdM_trW7S4dHOjIU'
-};
